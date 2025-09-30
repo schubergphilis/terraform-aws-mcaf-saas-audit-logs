2188d40d4f81e7440cd117670748b5fc2d9d3b97
Mayur Satpute <mayur.satpute@devolksbank.nl>
2025-09-30 10:25:05 +0200

    Adding a DLQ and replay mechanism for terraform-cloud

--- modules/audit-lambda/outputs.tf
+++ modules/audit-lambda/outputs.tf
@@ -2,3 +2,18 @@ output "arn" {
   description = "The ARN of the Lambda function"
   value       = module.lambda.arn
 }
+
+output "s3_lambda_package_object_key" {
+  description = "S3 Lambda package object key"
+  value       = aws_s3_object.lambda_package.key
+}
+
+output "s3_lambda_package_object_version" {
+  description = "S3 Lambda package object key"
+  value       = aws_s3_object.lambda_package.version_id
+}
+
+output "s3_lambda_package_object_checksum_sha256" {
+  description = "S3 Lambda package object checksum (sha256)"
+  value       = aws_s3_object.lambda_package.checksum_sha256
+}
--- terraform-cloud.tf
+++ terraform-cloud.tf
@@ -54,3 +54,43 @@ resource "aws_lambda_event_source_mapping" "terraform_audit_sqs_trigger" {
   event_source_arn = aws_sqs_queue.terraform_cloud_audit_log[0].arn
   function_name    = module.lambda["terraform-cloud"].arn
 }
+
+module "dlq_replay_lambda" {
+  source  = "schubergphilis/mcaf-lambda/aws"
+  version = "~> 1.4.1"
+
+  name                        = "terraform-cloud-dlq-replay"
+  create_policy               = true
+  create_s3_dummy_object      = false
+  description                 = "Lambda to replay messages from DLQ to main SQS queue for Terraform Cloud audit logs"
+  handler                     = "dlq_replay.handler"
+  kms_key_arn                 = var.kms_key_arn
+  log_retention               = var.lambda_log_retention
+  memory_size                 = 128
+  runtime                     = "python${var.python_version}"
+  s3_bucket                   = "${var.bucket_base_name}-lambda-${data.aws_caller_identity.current.account_id}"
+  s3_key                      = module.lambda["terraform-cloud"].s3_lambda_package_object_key
+  s3_object_version           = module.lambda["terraform-cloud"].s3_lambda_package_object_version
+  source_code_hash            = module.lambda["terraform-cloud"].s3_lambda_package_object_checksum_sha256
+  environment                 = {
+    MAIN_QUEUE_URL = try(aws_sqs_queue.terraform_cloud_audit_log[0].id, "")
+  }
+  subnet_ids                  = var.subnet_ids
+  security_group_egress_rules = var.security_group_egress_rules
+  tags                        = var.tags
+  timeout                     = 600
+
+  depends_on = [
+    module.bucket_for_lambda_package,
+    aws_sqs_queue.terraform_cloud_audit_log,
+    aws_sqs_queue.terraform_cloud_audit_log_dlq
+  ]
+}
+
+resource "aws_lambda_permission" "allow_dlq_to_invoke_dlq_replay_lambda" {
+  statement_id  = "AllowLambdaExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda.name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_sqs_queue.terraform_cloud_audit_log_dlq[0].arn
+}
